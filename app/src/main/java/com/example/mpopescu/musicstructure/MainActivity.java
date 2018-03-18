aca68952af3ff0391e56e7c76deff6880e76c830
Popescu Ionut Mihai <mihaineken@live.com>
2018-03-18 16:58:07 +0200

    working tabs

--- app/src/main/java/com/example/mpopescu/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/mpopescu/musicstructure/MainActivity.java
@@ -1,18 +1,24 @@
 /**
  * Music Structure app made for Udacity Android Basics by Google Nanodegree Program
- *
+ * <p>
  * I've used song list from www.billboard.com
  * This app also implements a click that direct the user to the specific web pages
- *
  */
 
 package com.example.mpopescu.musicstructure;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] tabNames = {R.string._70s_text_view,
+            R.string._80s_text_view,
+            R.string._90s_text_view,
+            R.string._00s_text_view};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(new HitsFragmentPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabNames.length; i++)
+            tabLayout.getTabAt(i).setText(tabNames[i]);
     }
 }
